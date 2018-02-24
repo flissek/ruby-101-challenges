76537ce810097f0261c6bfe52bdbb61ab2f825d7
flissek <flissek@gmail.com>
2018-02-25 00:48:59 +0100

    add answer to the 12 task (#14)

--- 12-Acronymize/lib/acronymize.rb
+++ 12-Acronymize/lib/acronymize.rb
@@ -1,4 +1,14 @@
+class String
+  def initial
+    self[0, 1]
+  end
+end
+
 def acronymize(sentence)
   # TODO: return the acronym on the given sentence
-
+  # sentence.gsub(/,*\s+/,',').to_a
+  a = sentence.upcase.split.map! do |x|
+    x.initial
+  end
+  a.join
 end
